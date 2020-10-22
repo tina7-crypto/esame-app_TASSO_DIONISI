731949d6ac068f184d19fd35101660af878d2f3f
martina <tassomartina@gmail.com>
2020-10-22 16:09:37 +0200

    Initializing repo 22 Oct
    Initializing repo 22 Oct

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Canon 50mm",
+    description: "Conveniente obiettivo di alta qualità, perfetto per ritratti creativi e fotografia in condizioni di scarsa illuminazione.",
+    imageFileName: "canon_50mm.jpg",
+    },
+});
